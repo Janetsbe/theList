e6b027b476a31b67fd67a9bacaf3942f5cd72606
Joey Anetsberger <joeyanetsberger@ip0af50bf1.int.uvm.edu>
2014-12-15 18:33:48 -0500

    Establish branch and begin UI design and learning time

--- theList/LoginViewController.swift
+++ theList/LoginViewController.swift
@@ -6,17 +6,64 @@
 //  Copyright (c) 2014 Joey Anetsberger. All rights reserved.
 //
 
-import UIKit
+import UIKit;
+import Foundation;
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController, FBLoginViewDelegate {
+    // IBOutlets
+    
+    @IBOutlet var facebookLoginView: FBLoginView!;
 
+    @IBOutlet var createProfileView: UIView!
+    
+    
+    // flags
+    var isDoneFetchedUser = false;
+    var wasLoggedOut = true;
+    
+    
+    // Inf
+    
+    
+
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        
+        self.facebookLoginView.delegate = self;
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        if(!isDoneFetchedUser) {
+            isDoneFetchedUser = true;
+//            createProfileView.hidden = false;
+            println(user.objectID);
+        }
+    }
+    
+    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+//
+    }
 
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        isDoneFetchedUser = false;
+    }
+    
+    
+    @IBAction func createProfileButtonPressed(sender: AnyObject) {
+        
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
+        
+        
         // Dispose of any resources that can be recreated.
     }
 
